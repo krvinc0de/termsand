54601586b6ab1c406a643d608a94c7a4342cae14
Kevin Medina <79812923+krvinc0de@users.noreply.github.com>
2023-04-04 14:13:40 -0600

    init
    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Viewer } from '@react-pdf-viewer/core';
+import { Worker } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css'
+import './styles.css'
+
+function App() {
+  return (
+      <div style={{
+          height: '100%',
+      }}>
+          <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js"></Worker>
+          <Viewer fileUrl="/docs/cookiespolicies.pdf" />
+      </div>
+  );
+}
+
+export default App;
